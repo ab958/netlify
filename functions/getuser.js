2f348b37aa55d8e80768400b0d056e41c340d7c4
Abdul Wahab <abwahab958@gmail.com>
2021-09-23 00:36:08 -0700

    final

---
+++ functions/getuser.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  const data = await axios("https://jsonplaceholder.typicode.com/posts");
+  console.log(context, "wahab");
+
+  if (event.httpMethod == "GET") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: data.data,
+      }),
+    };
+  } else if (event.httpMethod == "POST") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: "helloooo",
+      }),
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: "Hello ",
+    };
+  }
+};
